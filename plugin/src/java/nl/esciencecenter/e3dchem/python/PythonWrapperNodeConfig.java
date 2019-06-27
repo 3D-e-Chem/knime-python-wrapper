f1312d69b65c094b0b0fcddccd6777be4f5fdc2f
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-06-27 10:42:28 +0200

    Adapt to knime python2 extension interface changes

--- plugin/src/java/nl/esciencecenter/e3dchem/python/PythonWrapperNodeConfig.java
+++ plugin/src/java/nl/esciencecenter/e3dchem/python/PythonWrapperNodeConfig.java
@@ -1,14 +1,19 @@
 package nl.esciencecenter.e3dchem.python;
 
 import java.util.HashSet;
-import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 
+import org.knime.python2.DefaultPythonCommand;
+import org.knime.python2.PythonCommand;
+import org.knime.python2.PythonModuleSpec;
+import org.knime.python2.PythonVersion;
 import org.knime.python2.extensions.serializationlibrary.SentinelOption;
 import org.knime.python2.extensions.serializationlibrary.SerializationOptions;
 import org.knime.python2.generic.VariableNames;
 import org.knime.python2.kernel.PythonKernelOptions;
-import org.knime.python2.kernel.PythonKernelOptions.PythonVersionOption;
+import org.knime.python2.prefs.PythonPreferences;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
@@ -23,17 +28,49 @@ import org.knime.core.node.workflow.FlowVariable;
  */
 public class PythonWrapperNodeConfig {
 	private static final String CFG_PYTHON_VERSION_OPTION = "pythonVersionOption";
+    static final String CFG_PYTHON2COMMAND = "python2Command";
+    static final String CFG_PYTHON3COMMAND = "python3Command";
 	private static final String CFG_CONVERT_MISSING_TO_PYTHON = "convertMissingToPython";
 	private static final String CFG_CONVERT_MISSING_FROM_PYTHON = "convertMissingFromPython";
 	private static final String CFG_SENTINEL_OPTION = "sentinelOption";
 	private static final String CFG_SENTINEL_VALUE = "sentinelValue";
 	private static final String CFG_CHUNK_SIZE = "chunkSize";
 
+    /**
+     * {@link #m_python2Command} and {@link #m_python3Command} are special in that they currently aren't configurable
+     * via a Python scripting node's dialog but only using flow variables. If no respective flow variables are set,
+     * their value is retrieved from the Python preferences.
+     */
+    private static final String INDICATE_FALLBACK_TO_PREFERENCES_COMMAND_VALUE = "";
+
+    private PythonVersion m_pythonVersion = PythonPreferences.getPythonVersionPreference();
+
+    /**
+     * {@code null} means to fall back to {@link PythonPreferences#getPython2CommandPreference()}.
+     */
+    private PythonCommand m_python2Command = null;
+
+    /**
+     * {@code null} means to fall back to {@link PythonPreferences#getPython2CommandPreference()}.
+     */
+    private PythonCommand m_python3Command = null;
+
+    private int m_chunkSize = SerializationOptions.DEFAULT_CHUNK_SIZE;
+
+    private boolean m_convertMissingToPython = SerializationOptions.DEFAULT_CONVERT_MISSING_TO_PYTHON;
+
+    private boolean m_convertMissingFromPython = SerializationOptions.DEFAULT_CONVERT_MISSING_FROM_PYTHON;
+
+    private SentinelOption m_sentinelOption = SerializationOptions.DEFAULT_SENTINEL_OPTION;
+
+    private int m_sentinelValue = SerializationOptions.DEFAULT_SENTINEL_VALUE;
+    
 	protected final String pythonOptions = "options";
 	protected final String flowVariables = "flow_variables";
 	protected final String warningMessageFlowVariable = "warning_message";
 	protected final VariableNames variableNames;
-	private PythonKernelOptions kernelOptions = new PythonKernelOptions();
+	private String externalCustomPath = "";
+	private Set<PythonModuleSpec> additionalRequiredModules = new HashSet<PythonModuleSpec>();
 
 	/**
 	 * The variable names of the dataframes in the python workspace holding the input tables is set to `input_table`.
@@ -55,6 +92,145 @@ public class PythonWrapperNodeConfig {
 		return variableNames;
 	}
 
+    /**
+     * Indicates whether Python 3 shall be used by Python scripting nodes that are configured by this config instance.
+     *
+     * @return {@code true} if Python 3 shall be used, {@code false} if Python 2 shall be used.
+     */
+    public boolean getUsePython3() {
+        return m_pythonVersion.equals(PythonVersion.PYTHON3);
+    }
+
+    /**
+     * @return The configured Python version.
+     */
+    public PythonVersion getPythonVersion() {
+        return m_pythonVersion;
+    }
+
+    /**
+     * @param pythonVersion The configured Python version.
+     */
+    public void setPythonVersion(final PythonVersion pythonVersion) {
+        m_pythonVersion = pythonVersion;
+    }
+
+    /**
+     * @return The Python 2 command to use. May be {@code null} in which case no specific Python 2 command is configured
+     *         and one has to resort to - e.g., - the {@link PythonPreferences#getPython2CommandPreference() global
+     *         preferences}.
+     */
+    public PythonCommand getPython2Command() {
+        return m_python2Command;
+    }
+
+    /**
+     * @param python2Command The Python 2 command to use. May be {@code null} in which case no specific Python 2 command
+     *            is configured and one has to resort to - e.g., - the
+     *            {@link PythonPreferences#getPython2CommandPreference() global preferences}.
+     */
+    public void setPython2Command(final PythonCommand python2Command) {
+        m_python2Command = python2Command;
+    }
+
+    /**
+     * @return The Python 3 command to use. May be {@code null} in which case no specific Python 3 command is configured
+     *         and one has to resort to - e.g., - the {@link PythonPreferences#getPython3CommandPreference() global
+     *         preferences}.
+     */
+    public PythonCommand getPython3Command() {
+        return m_python3Command;
+    }
+
+    /**
+     * @param python3Command The Python 3 command to use. May be {@code null} in which case no specific Python 3 command
+     *            is configured and one has to resort to - e.g., - the
+     *            {@link PythonPreferences#getPython3CommandPreference() global preferences}.
+     */
+    public void setPython3Command(final PythonCommand python3Command) {
+        m_python3Command = python3Command;
+    }
+
+    /**
+    *
+    * @return The configured number of rows to transfer to/from Python per chunk of an input/output table.
+    */
+   public int getChunkSize() {
+       return m_chunkSize;
+   }
+
+   /**
+    *
+    * @param chunkSize The configured number of rows to transfer to/from Python per chunk of an input/output table.
+    */
+   public void setChunkSize(final int chunkSize) {
+       m_chunkSize = chunkSize;
+   }
+
+   /**
+    * @return {@true} if missing values shall be converted to sentinel values on the way to Python. {@code false} if
+    *         they shall remain missing.
+    */
+   public boolean getConvertMissingToPython() {
+       return m_convertMissingToPython;
+   }
+
+   /**
+    * @param convertMissingToPython {@true} to configure that missing values shall be converted to sentinel values on
+    *            the way to Python. {@code false} if they shall remain missing.
+    */
+   public void setConvertMissingToPython(final boolean convertMissingToPython) {
+       m_convertMissingToPython = convertMissingToPython;
+   }
+
+   /**
+    * @return {@true} if missing values shall be converted to sentinel values on the way back from Python.
+    *         {@code false} if they shall remain missing.
+    */
+   public boolean getConvertMissingFromPython() {
+       return m_convertMissingFromPython;
+   }
+
+   /**
+    * @param convertMissingFromPython {@true} to configure that missing values shall be converted to sentinel values on
+    *            the way back from Python. {@code false} if they shall remain missing.
+    */
+   public void setConvertMissingFromPython(final boolean convertMissingFromPython) {
+       m_convertMissingFromPython = convertMissingFromPython;
+   }
+
+   /**
+    * @return The configured sentinel options to use (if applicable; see {@link #getConvertMissingToPython()} and
+    *         {@link #getConvertMissingFromPython()}).
+    */
+   public SentinelOption getSentinelOption() {
+       return m_sentinelOption;
+   }
+
+   /**
+    * @param sentinelOption The configured sentinel options to use (if applicable; see
+    *            {@link #getConvertMissingToPython()} and {@link #getConvertMissingFromPython()}).
+    */
+   public void setSentinelOption(final SentinelOption sentinelOption) {
+       m_sentinelOption = sentinelOption;
+   }
+
+   /**
+    * @return The configured sentinel value to use (only used if {@link #getSentinelOption()} is
+    *         {@link SentinelOption#CUSTOM}).
+    */
+   public int getSentinelValue() {
+       return m_sentinelValue;
+   }
+
+   /**
+    * @param sentinelValue The configured sentinel value to use (only used if {@link #getSentinelOption()} is
+    *            {@link SentinelOption#CUSTOM}).
+    */
+   public void setSentinelValue(final int sentinelValue) {
+       m_sentinelValue = sentinelValue;
+   }
+    
 	/**
 	 * Variable name inside Python script which is set to a dictionary filled by
 	 * {@link #getOptionsValues()}.
@@ -93,13 +269,15 @@ public class PythonWrapperNodeConfig {
 	 *            The settings to save to
 	 */
 	public void saveToInDialog(NodeSettingsWO settings) {
-		settings.addString(CFG_PYTHON_VERSION_OPTION, kernelOptions.getPythonVersionOption().name());
-		settings.addBoolean(CFG_CONVERT_MISSING_TO_PYTHON, kernelOptions.getConvertMissingToPython());
-		settings.addBoolean(CFG_CONVERT_MISSING_FROM_PYTHON, kernelOptions.getConvertMissingFromPython());
-		settings.addString(CFG_SENTINEL_OPTION, kernelOptions.getSentinelOption().name());
-		settings.addInt(CFG_SENTINEL_VALUE, kernelOptions.getSentinelValue());
-		settings.addInt(CFG_CHUNK_SIZE, kernelOptions.getChunkSize());
-	}
+        settings.addString(CFG_PYTHON_VERSION_OPTION, getPythonVersion().getId());
+        settings.addString(CFG_PYTHON2COMMAND, commandToString(getPython2Command()));
+        settings.addString(CFG_PYTHON3COMMAND, commandToString(getPython3Command()));
+        settings.addInt(CFG_CHUNK_SIZE, getChunkSize());
+        settings.addBoolean(CFG_CONVERT_MISSING_TO_PYTHON, getConvertMissingToPython());
+        settings.addBoolean(CFG_CONVERT_MISSING_FROM_PYTHON, getConvertMissingFromPython());
+        settings.addString(CFG_SENTINEL_OPTION, getSentinelOption().name());
+        settings.addInt(CFG_SENTINEL_VALUE, getSentinelValue());
+   	}
 	
 	/**
 	 * Load configuration from the given node settings.
@@ -121,19 +299,37 @@ public class PythonWrapperNodeConfig {
 	 *            The settings to load from
 	 */
 	public void loadFromInDialog(final NodeSettingsRO settings) {
-		kernelOptions.setPythonVersionOption(PythonVersionOption.valueOf(
-				settings.getString(CFG_PYTHON_VERSION_OPTION, kernelOptions.getPreferencePythonVersion().name())));
-		kernelOptions.setConvertMissingToPython(settings.getBoolean(CFG_CONVERT_MISSING_TO_PYTHON,
-				SerializationOptions.DEFAULT_CONVERT_MISSING_TO_PYTHON));
-		kernelOptions.setConvertMissingFromPython(settings.getBoolean(CFG_CONVERT_MISSING_FROM_PYTHON,
-				SerializationOptions.DEFAULT_CONVERT_MISSING_FROM_PYTHON));
-		kernelOptions.setSentinelOption(SentinelOption
-				.valueOf(settings.getString(CFG_SENTINEL_OPTION, SerializationOptions.DEFAULT_SENTINEL_OPTION.name())));
-		kernelOptions
-				.setSentinelValue(settings.getInt(CFG_SENTINEL_VALUE, SerializationOptions.DEFAULT_SENTINEL_VALUE));
-		kernelOptions.setChunkSize(settings.getInt(CFG_CHUNK_SIZE, PythonKernelOptions.DEFAULT_CHUNK_SIZE));
+		final String pythonVersionString = settings.getString(CFG_PYTHON_VERSION_OPTION, getPythonVersion().getId());
+        // Backward compatibility: old saved versions may be all upper case.
+        setPythonVersion(PythonVersion.fromId(pythonVersionString.toLowerCase(Locale.ROOT)));
+        final String python2CommandString =
+            settings.getString(CFG_PYTHON2COMMAND, commandToString(getPython2Command()));
+        setPython2Command(commandFromString(python2CommandString));
+        final String python3CommandString =
+            settings.getString(CFG_PYTHON3COMMAND, commandToString(getPython3Command()));
+        setPython3Command(commandFromString(python3CommandString));
+        setChunkSize(settings.getInt(CFG_CHUNK_SIZE, getChunkSize()));
+        setConvertMissingToPython(settings.getBoolean(CFG_CONVERT_MISSING_TO_PYTHON, getConvertMissingToPython()));
+        setConvertMissingFromPython(
+            settings.getBoolean(CFG_CONVERT_MISSING_FROM_PYTHON, getConvertMissingFromPython()));
+        setSentinelOption(SentinelOption.valueOf(settings.getString(CFG_SENTINEL_OPTION, getSentinelOption().name())));
+        setSentinelValue(settings.getInt(CFG_SENTINEL_VALUE, getSentinelValue()));
 	}
 
+    private static String commandToString(final PythonCommand command) {
+        return command != null //
+            ? command.toString() //
+            : INDICATE_FALLBACK_TO_PREFERENCES_COMMAND_VALUE;
+    }
+
+    private static PythonCommand commandFromString(final String commandString) {
+        return Objects.equals(commandString, INDICATE_FALLBACK_TO_PREFERENCES_COMMAND_VALUE) //
+            ? null //
+            // TODO: This only works for ordinary paths ("manual configuration"), not for Conda directory + environment
+            // name ("Conda configuration").
+            : new DefaultPythonCommand(commandString);
+    }
+    
 	/**
 	 * Set of key/value pairs which inside Python script will be a dictionary
 	 * named by {@link #getOptionsName()}.
@@ -145,43 +341,18 @@ public class PythonWrapperNodeConfig {
 		return variables;
 	}
 
-	/**
-	 * Sets the internal {@link PythonKernelOptions} to a new object created
-	 * using the specified parameters.
-	 *
-	 * @param versionOption
-	 *            the version options
-	 * @param convertToPython
-	 *            convert missing values to sentinel on the way to python
-	 * @param convertFromPython
-	 *            convert sentinel to missing values on the way from python to
-	 *            KNIME
-	 * @param sentinelOption
-	 *            the sentinel option
-	 * @param sentinelValue
-	 *            the sentinel value (only used if sentinelOption is CUSTOM)
-	 * @param chunkSize
-	 *            the number of rows to transfer per chunk
-	 */
-	public void setKernelOptions(final PythonVersionOption versionOption, final boolean convertToPython,
-			final boolean convertFromPython, final SentinelOption sentinelOption, final int sentinelValue,
-			final int chunkSize) {
-		List<String> requiredModules = kernelOptions.getAdditionalRequiredModules();
-		kernelOptions = new PythonKernelOptions(versionOption, convertToPython, convertFromPython, sentinelOption,
-				sentinelValue, chunkSize);
-		for (String requiredModule : requiredModules) {
-			kernelOptions.addRequiredModule(requiredModule);
-		}
-	}
-
-	/**
-	 * Gets the python kernel options.
-	 *
-	 * @return the python kernel options
-	 */
-	public PythonKernelOptions getKernelOptions() {
-		return new PythonKernelOptions(kernelOptions);
-	}
+    /**
+     * Creates and returns a new {@link PythonKernelOptions} instance from the values stored in this config.
+     *
+     * @return The Python kernel options.
+     */
+    public PythonKernelOptions getKernelOptions() {
+        final SerializationOptions serializationOptions = new SerializationOptions(getChunkSize(),
+            getConvertMissingToPython(), getConvertMissingFromPython(), getSentinelOption(), getSentinelValue());
+		PythonKernelOptions opt = new PythonKernelOptions(getPythonVersion(), getPython2Command(), getPython3Command(),
+            serializationOptions);
+		return opt.forAddedAdditionalRequiredModules(additionalRequiredModules).forExternalCustomPath(externalCustomPath);
+    }
 
 	/**
 	 * Add an additional required module. A check for that module is performed
@@ -190,6 +361,23 @@ public class PythonWrapperNodeConfig {
 	 * @param name
 	 */
 	public void addRequiredModule(String name) {
-		kernelOptions.addRequiredModule(name);
+		addRequiredModule(new PythonModuleSpec(name));
+	}
+
+	/**
+	 * Add an additional required module. A check for that module is performed
+	 * on node execution.
+	 * @param spec 
+	 */
+	public void addRequiredModule(PythonModuleSpec spec) {
+		additionalRequiredModules.add(spec);
+	}
+	
+	/**
+	 * 
+	 * @param externalCustomPath The external custom path to set. It will be appended to the Python kernel's PYTHONPATH.
+	 */
+	public void setExternalCustomPath(String externalCustomPath) {
+		this.externalCustomPath = externalCustomPath;
 	}
 }
